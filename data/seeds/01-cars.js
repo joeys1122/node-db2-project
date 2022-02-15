deb4c34c7462a19a7b858af072fa82a853e009d3
joeys1122 <josephs1122@gmail.com>
2022-02-15 15:37:20 -0500

    added car seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,10 @@
-// STRETCH
+exports.seed = function(knex) {
+  return knex('cars').truncate()
+    .then(function() {
+      return knex('cars').insert([
+        { vin: '3C4FY58B95T580963', make: 'Nissan', model: 'Sentra', mileage: 12000, title: 'Test Title' },
+        { vin: '2CNALFEC6B6392134', make: 'Toyoda', model: 'Takoma', mileage: 12000, transmission: 'Test Transmission' },
+        { vin: '3D7KS28C86G285957', make: 'Audi', model: 'A3', mileage: 12000 }
+      ]);
+    });
+};
