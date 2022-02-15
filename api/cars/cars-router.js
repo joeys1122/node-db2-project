3edcc354fc8ae8af3c9d66205833a91c3861593d
joeys1122 <josephs1122@gmail.com>
2022-02-15 14:29:51 -0500

    added cars router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,37 @@
-// DO YOUR MAGIC
+const Cars = require('./cars-model');
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+  Cars.getAll()
+    .then(cars => {
+      res.json(cars)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Error getting cars' });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  Cars.getById(req.params.id)
+    .then(car => {
+      res.json(car)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Error getting error car with specific id'});
+    });
+});
+
+router.post('/', (req, res) => {
+  Cars.create(req.body)
+    .then(car => {
+      res.status(201).json(car);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Error posting car' });
+    });
+});
+
+module.exports = router;
